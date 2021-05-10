02944758864d65b92f8b384cbd73f83d6362b6ea
ionutcaramaliu <83408716+ionutcaramaliu@users.noreply.github.com>
2021-05-10 11:52:19 +0300

    Update server.js

--- task7/server.js
+++ task7/server.js
@@ -7,10 +7,10 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
- host: ,
- user: ,
- password: ,
- database: 
+ host: localhost,
+ user: user,
+ password: pass,
+ database: test
 });
 con.connect(function(err) {
  if (err) throw err;
